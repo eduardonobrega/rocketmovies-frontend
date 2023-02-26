74ab41a9df4fdc59d7d1a019b71ddbefad4fbe1a
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-02-26 08:16:13 -0300

    Profile

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,51 @@
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
+
+import { Container, Header, Form, Avatar } from './styles';
+
+import { ButtonText } from '../../components/ButtonText';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
+export function Profile() {
+  return (
+    <Container>
+      <Header>
+        <a to={-1}>
+          <ButtonText icon={FiArrowLeft} text="Voltar" />
+        </a>
+      </Header>
+
+      <Form>
+        <Avatar>
+          <img
+            src="https://github.com/eduardonobrega.png"
+            alt={`Foto de perfil de Eduardo`}
+          />
+
+          <label htmlFor="avatar">
+            <FiCamera />
+
+            <input type="file" id="avatar" className="sr-only" />
+          </label>
+        </Avatar>
+
+        <Input type="text" icon={FiUser} placeholder="Nome" />
+        <Input type="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          type="password"
+          id="oldPassword"
+          icon={FiLock}
+          placeholder="Senha atual"
+        />
+        <Input
+          type="password"
+          id="newPassword"
+          icon={FiLock}
+          placeholder="Nova senha"
+        />
+
+        <Button title="Salvar" />
+      </Form>
+    </Container>
+  );
+}
