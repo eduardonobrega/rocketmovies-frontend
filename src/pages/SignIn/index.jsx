783984e4a6cabbd6f36f7feb8c8e78840522f49b
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-02-26 09:22:06 -0300

    Page SignIn

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,33 @@
+import { FiMail, FiLock } from 'react-icons/fi';
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+import { ButtonText } from '../../components/ButtonText';
+
+import { Container, Background, Form } from './styles';
+
+export function SignIn() {
+  return (
+    <Container>
+      <main>
+        <Form>
+          <h1>RocketMovies</h1>
+          <p>Aplicação para acompanhar tudo que assistir.</p>
+          <h2>Faça seu login</h2>
+
+          <Input type="email" placeholder="E-mail" icon={FiMail} />
+
+          <Input type="password" placeholder="Senha" icon={FiLock} />
+
+          <Button title="Entrar" />
+
+          <a to="/register">
+            <ButtonText type="button" text="Criar conta" />
+          </a>
+        </Form>
+      </main>
+
+      <Background />
+    </Container>
+  );
+}
