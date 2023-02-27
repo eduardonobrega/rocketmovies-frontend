0e4ef813d559d0164b07f3a19c1f1ac145c0d994
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-02-26 21:34:00 -0300

    fetch tags home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,8 @@
 import { FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
+import { api } from '../../services/api';
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
@@ -9,6 +11,15 @@ import { Note } from '../../components/Note';
 import { Container, Content } from './styles';
 
 export function Home() {
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get('/tags');
+      setTags(response.data);
+    }
+    fetchTags();
+  }, []);
   return (
     <Container>
       <Header />
@@ -16,9 +27,12 @@ export function Home() {
         <h1>Meus filmes</h1>
 
         <select>
-          <option value="">Todos</option>
-          <option value="Capitão America">Capitão America</option>
-          <option value="Aventura">Aventura</option>
+          <option value="all">Todos</option>
+          {tags.map((tag) => (
+            <option key={tag.id} value={tag.name}>
+              {tag.name}
+            </option>
+          ))}
         </select>
 
         <Link to="/new">
@@ -35,7 +49,7 @@ export function Home() {
               rating: 5,
               description:
                 'Um promissor romancista, Mike Enslin (John Cusack), resolveu enveredar por outro caminho e escrever livros que investiguem fenômenos paranormais. Enslin nunca presenciou realmente algum destes fatos, então fica difícil obter credibilidade. Além do mais Mike é totalmente cético, pois até hoje não encontrou evidências de que exista vida após a morte. No entanto decide ir até Nova York e se hospedar no Dolphin Hotel, mais exatamente no quarto 1408, que tem fama de ser habitado por espíritos malignos. O gerente do hotel, Gerald Olin (Samuel L. Jackson), o avisa que 56 mortes já ocorreram neste quarto, mas Mike está decidido a conferir se sua fama está condizente com a verdade.',
-              tags: [{id: 1, name: 'Terror'}],
+              tags: [{ id: 1, name: 'Terror' }],
             }}
           />
         </Content>
